2df6470939dc8160701f44ab1e0dcf94e7e3c4b8
Vadym QA <vadim0604.and@gmail.com>
2023-09-20 17:34:45 +0300

    add hm_3

---
+++ test_hm.py
@@ -0,0 +1,29 @@
+enter = input("enter_text:")
+remove = input("remove_text:")
+result_one = enter.replace(remove,'')
+print(result_one)
+# one task
+high = input("high_text:")
+result_two = high.title()
+print(result_two)
+# second task
+result_three = input()
+print(result_three[::-1])
+# three task
+string_one = input('first_word:')
+string_two = input('second_word:')
+print(string_one == string_two)
+# four task
+result_six = input('word:')
+result_seven = int(input('#:'))
+print(result_six * result_seven)
+# five task
+uah_usd = input('uah_to_usd:')
+print(int(uah_usd) / 37)
+usd_uah = input('usd_to_uah:')
+print(int(usd_uah) * 37)
+uah_euro = input('uah_to_euro:')
+print(int(uah_euro) / 40)
+euro_uah = input('euro_to_uah:')
+print(int(euro_uah) * 40)
+# six task
