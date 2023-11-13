cffd80f4b9ac6401ba92b2d99ff5c9c8f92d05ef
Vadym QA <vadim.andriienko@sweetstackdigital.com>
2023-11-13 21:59:49 +0200

    refactor

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,32 +1,36 @@
 import pytest
 from src import my_func
 
-def test_add_one():
-    assert my_func.addition(3,4) == 7
+@pytest.mark.parametrize("a, b, c", [(1, 2, 3)])
+def test_add_one(a,b,c):
+    assert my_func.addition(a,b) == c
 def test_add_two():
     assert my_func.addition(5,5) == 10
 @pytest.mark.skip('Negative test')
 def test_add_three():
     assert my_func.addition(3,3) == 8
 
-def test_sub_one():
-    assert my_func.subtraction(10,2) == 8
+@pytest.mark.parametrize("a, b, c", [(5, 2, 3)])
+def test_sub_one(a,b,c):
+    assert my_func.subtraction(a,b) == c
 def test_sub_two():
     assert my_func.subtraction(50,2) == 48
 @pytest.mark.skip('Negative test')
 def test_sub_three():
         assert my_func.subtraction(10, 2) == 5
 
-def test_mul_one():
-    assert my_func.multiplication(1,2) == 2
+@pytest.mark.parametrize("a, b, c", [(5, 2, 10)])
+def test_mul_one(a,b,c):
+    assert my_func.multiplication(a,b) == c
 def test_mul_two():
     assert my_func.multiplication(3,2) == 6
 @pytest.mark.skip('Negative test')
 def test_mul_three():
     assert my_func.multiplication(3,2) == 8
 
-def test_div_one():
-    assert my_func.division(10,2) == 5
+@pytest.mark.parametrize("a, b, c", [(10, 2, 5)])
+def test_div_one(a,b,c):
+    assert my_func.division(a,b) == c
 def test_div_two():
     assert my_func.division(100,2) == 50
 @pytest.mark.skip('Negative test')
